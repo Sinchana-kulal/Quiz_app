87f5f6c1f014514c290cd296f9a5fed79776914c
Sinchana Kulal <sinchanakulal1246@gmail.com>
2026-02-13 09:39:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Quiz from "./components/Quiz";
+import questions from "./data";
+
+import "./App.css";
+
+function App() {
+  return <Quiz />;
+}
+
+export default App;
---
+++ Data.jsx
@@ -0,0 +1,114 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is React?",
+    options: [
+      "A JavaScript library",
+      "A database",
+      "A browser",
+      "An operating system"
+    ],
+    answer: "A JavaScript library"
+  },
+  {
+    id: 2,
+    question: "React is mainly used to build?",
+    options: [
+      "User Interfaces",
+      "Servers",
+      "Databases",
+      "Games"
+    ],
+    answer: "User Interfaces"
+  },
+  {
+    id: 3,
+    question: "Who developed React?",
+    options: [
+      "Google",
+      "Facebook",
+      "Microsoft",
+      "Amazon"
+    ],
+    answer: "Facebook"
+  },
+  {
+    id: 4,
+    question: "What is JSX?",
+    options: [
+      "JavaScript XML",
+      "Java Syntax",
+      "JSON File",
+      "Java Code"
+    ],
+    answer: "JavaScript XML"
+  },
+  {
+    id: 5,
+    question: "Which hook is used to manage state?",
+    options: [
+      "useEffect",
+      "useState",
+      "useRef",
+      "useMemo"
+    ],
+    answer: "useState"
+  },
+  {
+    id: 6,
+    question: "Which hook is used for side effects?",
+    options: [
+      "useState",
+      "useEffect",
+      "useMemo",
+      "useReducer"
+    ],
+    answer: "useEffect"
+  },
+  {
+    id: 7,
+    question: "Props are used to?",
+    options: [
+      "Pass data between components",
+      "Store database",
+      "Style components",
+      "Create server"
+    ],
+    answer: "Pass data between components"
+  },
+  {
+    id: 8,
+    question: "React components must return?",
+    options: [
+      "HTML",
+      "JSX",
+      "JSON",
+      "CSS"
+    ],
+    answer: "JSX"
+  },
+  {
+    id: 9,
+    question: "Virtual DOM improves?",
+    options: [
+      "Security",
+      "Performance",
+      "Database speed",
+      "Network speed"
+    ],
+    answer: "Performance"
+  },
+  {
+    id: 10,
+    question: "Which function is used to create React app root in React 18?",
+    options: [
+      "ReactDOM.render()",
+      "createRoot()",
+      "renderApp()",
+      "mountRoot()"
+    ],
+    answer: "createRoot()"
+  } 
+];
+
+export default questions;
